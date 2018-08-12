03c4111c582318a69a1e9841b9240e36768de2bb
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2018-08-11 21:38:59 -0300

    [UPDATE] Headers

--- Servers/eAmob-A Rev-471/eAthena (Stable)/src/map/itemdb.h
+++ Servers/eAmob-A Rev-471/eAthena (Stable)/src/map/itemdb.h
@@ -1,5 +1,20 @@
-// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
-// For more information, see LICENCE in the main folder
+/****************************************************************************!
+*                            _                                               *
+*                           / \                         _                    *
+*                   ___    / _ \   _ __ ___   ____  ___| |                   *
+*                  / _ \  / /_\ \ | '_ ` _ \./  _ \/  _  |                   *
+*                 |  __/ /  ___  \| | | | | |  (_) ) (_) |                   *
+*                  \___|/__/   \__\_| |_| |_|\____/\_____/                   *
+*                                                                            *
+*                               eAmod Script                                 *
+*                                                                            *
+* \file src/map/itemdb.h                                                     *
+*                                                                            *
+* Primary Description.                                                       *
+* More elaborate description of the file.                                    *
+* \author eAthena Dev Teams                                                  *
+* \date ?                                                                    *
+*****************************************************************************/
 
 #ifndef _ITEMDB_H_
 #define _ITEMDB_H_
