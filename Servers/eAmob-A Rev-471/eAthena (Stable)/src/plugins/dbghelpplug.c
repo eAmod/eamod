cfa35d7425495c5fa700ec6db3c5849c0a334402
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2018-08-11 22:00:54 -0300

    [UPDATE] Headers

--- Servers/eAmob-A Rev-471/eAthena (Stable)/src/plugins/dbghelpplug.c
+++ Servers/eAmob-A Rev-471/eAthena (Stable)/src/plugins/dbghelpplug.c
@@ -1,13 +1,25 @@
+/****************************************************************************!
+*                            _                                               *
+*                           / \                         _                    *
+*                   ___    / _ \   _ __ ___   ____  ___| |                   *
+*                  / _ \  / /_\ \ | '_ ` _ \./  _ \/  _  |                   *
+*                 |  __/ /  ___  \| | | | | |  (_) ) (_) |                   *
+*                  \___|/__/   \__\_| |_| |_|\____/\_____/                   *
+*                                                                            *
+*                               eAmod Script                                 *
+*                                                                            *
+* \file src/plugins/dbghelpplug.c                                            *
+*                                                                            *
+* Primary Description.                                                       *
+* The author disclaims copyright to this source code.  In place of           *
+* a legal notice, here is a blessing:                                        *
+*    May you do good and not evil.					     * 
+*    May you find forgiveness for yourself and forgive others.		     * 
+*    May you share freely, never taking more than you give.		     *  
+* \author ? 		                                                     *
+* \date 2008 January 19                                                      *
+*****************************************************************************/
 /*
- * 2008 January 19
- *
- * The author disclaims copyright to this source code.  In place of
- * a legal notice, here is a blessing:
- *
- *    May you do good and not evil.
- *    May you find forgiveness for yourself and forgive others.
- *    May you share freely, never taking more than you give.
- *
  ********************************************************************
  *
  * DONE:
