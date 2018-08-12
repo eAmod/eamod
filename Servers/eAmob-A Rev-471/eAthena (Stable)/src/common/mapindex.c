192c7f4684585500d74fa8f4effc9063cb8aaa8b
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2018-08-11 21:19:48 -0300

    [UPDATE] Headers

--- Servers/eAmob-A Rev-471/eAthena (Stable)/src/common/mapindex.c
+++ Servers/eAmob-A Rev-471/eAthena (Stable)/src/common/mapindex.c
@@ -1,5 +1,20 @@
-// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
-// For more information, see LICENCE in the main folder
+/****************************************************************************!
+*                            _                                               *
+*                           / \                         _                    *
+*                   ___    / _ \   _ __ ___   ____  ___| |                   *
+*                  / _ \  / /_\ \ | '_ ` _ \./  _ \/  _  |                   *
+*                 |  __/ /  ___  \| | | | | |  (_) ) (_) |                   *
+*                  \___|/__/   \__\_| |_| |_|\____/\_____/                   *
+*                                                                            *
+*                               eAmod Script                                 *
+*                                                                            *
+* \file src/common/mapindex.c                                                *
+*                                                                            *
+* Primary Description.                                                       *
+* More elaborate description of the file.                                    *
+* \author eAthena Dev Teams                                                  *
+* \date ?                                                                    *
+*****************************************************************************/
 
 #include "../common/mmo.h"
 #include "../common/showmsg.h"
