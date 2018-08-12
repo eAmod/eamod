e0c91aff1e16c32236bb5d49a1965ef607cfbd75
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2018-08-11 21:04:41 -0300

    [UPDATE] Headers

--- Servers/eAmob-A Rev-471/eAthena (Stable)/src/char_sql/int_quest.h
+++ Servers/eAmob-A Rev-471/eAthena (Stable)/src/char_sql/int_quest.h
@@ -1,5 +1,20 @@
-// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
-// For more information, see LICENCE in the main folder
+/****************************************************************************!
+*                            _                                               *
+*                           / \                         _                    *
+*                   ___    / _ \   _ __ ___   ____  ___| |                   *
+*                  / _ \  / /_\ \ | '_ ` _ \./  _ \/  _  |                   *
+*                 |  __/ /  ___  \| | | | | |  (_) ) (_) |                   *
+*                  \___|/__/   \__\_| |_| |_|\____/\_____/                   *
+*                                                                            *
+*                               eAmod Script                                 *
+*                                                                            *
+* \file src/char_sql/int_quest.h                                             *
+*                                                                            *
+* Primary Description.                                                       *
+* More elaborate description of the file.                                    *
+* \author eAthena Dev Teams                                                  *
+* \date ?                                                                    *
+*****************************************************************************/
 
 #ifndef _QUEST_H_
 #define _QUEST_H_
